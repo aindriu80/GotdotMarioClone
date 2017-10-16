337d51176a431e9916e79a1c09ebbacce0c519ae
aindriu80 <aindriu80@gmail.com>
2017-10-16 10:34:52 +0100

    Setting up the Jump and Player Collisions

--- player.gd
+++ player.gd
@@ -1,20 +1,23 @@
 extends KinematicBody2D
 #Variables
-var speed = 0
-var velocity = 0
+var speedX = 0
+var speedY = 0
+var velocity = Vector2(0,0)
 var facingDirection = 0
 var movementDirection = 0
 #Constants
 const MAXIMUMSPEED = 300
 const MOVMULTI = 600
-
+const JUMPFORCE = 350
+const GRAVITY = 1000
 
 func _ready():
 	set_process(true)
 	pass
 	
 func _process(delta):
-	
+	if(Input.is_action_pressed("move_jump")):
+		speedY = -JUMPFORCE
 	if(Input.is_action_pressed("move_left")):
 		facingDirection = -1
 	elif(Input.is_action_pressed("move_right")):
@@ -22,10 +25,17 @@ func _process(delta):
 	else: facingDirection =0
 	
 	if(facingDirection!=0):
-		speed += MOVMULTI * delta
+		speedX += MOVMULTI * delta
 		movementDirection = facingDirection
-	else: speed -= MOVMULTI * 1.5 * delta
-	speed = clamp( speed, 0, MAXIMUMSPEED)
-	velocity = speed * delta * movementDirection
-	move(Vector2(velocity, 0))
+	else: speedX -= MOVMULTI * 1.5 * delta
+	speedX = clamp( speedX, 0, MAXIMUMSPEED)
+	speedY += GRAVITY * delta
+	velocity.x = speedX * delta * movementDirection
+	velocity.y = speedY * delta
+	var moveRemainder = move(velocity)
+	if is_colliding():
+		var normal = get_collision_normal()
+		var finalMov = normal.slide(moveRemainder)
+		speedY = 0
+		move(finalMov)
 	pass
